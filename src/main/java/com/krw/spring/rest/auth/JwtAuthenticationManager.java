8f521dfe314fb6a59f0c4a026642d261325dc58c
Rokwoon Kim <k862390@gmail.com>
2018-04-06 15:30:39 +0900

    Modify the exception type of AuthenticationManager to the appropriate type.

--- src/main/java/com/krw/spring/rest/auth/JwtAuthenticationManager.java
+++ src/main/java/com/krw/spring/rest/auth/JwtAuthenticationManager.java
@@ -3,11 +3,11 @@ package com.krw.spring.rest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.common.exceptions.UnauthorizedUserException;
+import org.springframework.security.oauth2.common.exceptions.UserDeniedAuthorizationException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -16,6 +16,7 @@ import com.krw.spring.service.UserService;
 import com.krw.spring.service.domain.User;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureException;
 
@@ -27,10 +28,17 @@ public class JwtAuthenticationManager implements AuthenticationManager {
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		/**
+		 * Throwing exceptions are not AuthenticationException.
+		 * OAuth2AuthenticationProcessingFilter doesn't catch the AuthenticationException.
+		 * It only catch OAuth2Exception. So this manager also should throws OAuth2Exception too.
+		 * See OAuth2AuthenticationProcessingFilter.doFilter
+		 */
+		
 		// BearerTokenExtractor sets token on principal.
 		// See OAuth2AuthenticationProcessingFilter.
 		if (StringUtils.isEmpty(authentication.getPrincipal())) {
-			throw new BadCredentialsException("Bad token");
+			throw new UnauthorizedUserException("Bad token");
 		}
 		String token = authentication.getPrincipal().toString();
 
@@ -41,12 +49,14 @@ public class JwtAuthenticationManager implements AuthenticationManager {
 			User user = (User) userService.loadUserByUsername(claims.getAudience());
 			
 			if (user == null) {
-				throw new UsernameNotFoundException(HttpStatus.NOT_FOUND.getReasonPhrase());
+				throw new UnauthorizedUserException(HttpStatus.NOT_FOUND.getReasonPhrase());
 			}
 
 			return new UsernamePasswordAuthenticationToken(user.getUuid(), user.getPassword(), user.getAuthorities());
 		} catch (final SignatureException e) {
-			throw new BadCredentialsException(HttpStatus.UNAUTHORIZED.getReasonPhrase());
+			throw new UnauthorizedUserException(HttpStatus.UNAUTHORIZED.getReasonPhrase());
+		} catch (ExpiredJwtException e) {
+			throw new UserDeniedAuthorizationException("Token expired.");
 		}
 	}
 

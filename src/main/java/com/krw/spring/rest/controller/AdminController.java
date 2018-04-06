6d8702e58264a1e6daa830a53aa486575a226baf
Rokwoon Kim <k862390@gmail.com>
2018-04-06 15:39:00 +0900

    Move retrieving all user info API to admin controller.

--- src/main/java/com/krw/spring/rest/controller/AdminController.java
+++ src/main/java/com/krw/spring/rest/controller/AdminController.java
@@ -1,6 +1,14 @@
 package com.krw.spring.rest.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.krw.spring.service.UserService;
+import com.krw.spring.service.domain.User;
 
 /**
  * administration API
@@ -9,5 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class AdminController {
-	// TODO define
+	
+	@Autowired
+	private UserService userService;
+	
+	@JsonView(User.View.Admin.class)
+	@GetMapping("/users")
+	public List<User> allUsers() {
+		return userService.showAll();
+	}
+	
 }
